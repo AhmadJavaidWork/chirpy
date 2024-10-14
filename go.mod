3aac2a4383adca38b199b811a8a6eb7dbaec716b
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-10-14 21:58:17 +0500

    server init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ahmadjavaidwork/chirpy
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz/", handlerReadiness)
+	srv := &http.Server{
+		Addr:        ":" + port,
+		Handler:     mux,
+		IdleTimeout: 2 * time.Second,
+	}
+
+	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
