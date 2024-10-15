7324131ff0bb616068274b5a0e1936365a759b91
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-10-15 21:46:32 +0500

    refactor

--- main.go
+++ main.go
@@ -19,18 +19,22 @@ type apiConfig struct {
 }
 
 func main() {
-	godotenv.Load()
-	dbURL := os.Getenv("DB_URL")
-	db, err := sql.Open("postgres", dbURL)
-	if err != nil {
-		log.Fatalf("cannot connect to db: %w", err)
-	}
-
 	const filepathRoot = "."
 	const port = "8080"
+
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
-		db:             database.New(db),
+		db:             database.New(dbConn),
 	}
 	mux := http.NewServeMux()
 	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
